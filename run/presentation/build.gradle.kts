2e20d939eb6e52fb6155513adcbf8272269633b7
Darpan <darpan.kapadia@gmail.com>
2024-07-17 18:14:51 +1200

    Setup version catalog and dependencies for each module

--- run/presentation/build.gradle.kts
+++ run/presentation/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.compose.compiler)
 }
+
 
 android {
     namespace = "com.dk.run.presentation"
@@ -33,11 +35,8 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.coil.compose)
+    implementation(libs.google.maps.android.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.timber)
 }
