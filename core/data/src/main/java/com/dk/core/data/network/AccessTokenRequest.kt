7e9456087417386531706aaac39ce2709ba1a1e0
Darpan <darpan.kapadia@gmail.com>
2024-08-18 12:52:37 +1200

    Setup token refresh mechanism

---
+++ core/data/src/main/java/com/dk/core/data/network/AccessTokenRequest.kt
@@ -0,0 +1,9 @@
+package com.dk.core.data.network
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessTokenRequest(
+    val refreshToken: String,
+    val userId: String
+)
