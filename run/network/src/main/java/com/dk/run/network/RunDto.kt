e49795614aca1020261e0f0c3ba398bf3302d24b
Darpan <darpan.kapadia@gmail.com>
2024-09-22 08:21:55 +1200

    Implemented run API

---
+++ run/network/src/main/java/com/dk/run/network/RunDto.kt
@@ -0,0 +1,17 @@
+package com.dk.run.network
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RunDto(
+    val id: String,
+    val dateTimeUtc: String,
+    val durationMillis: Long,
+    val distanceMeters: Int,
+    val lat: Double,
+    val long: Double,
+    val mapPictureUrl: String?,
+    val avgSpeedKmh: Double,
+    val maxSpeedKmh: Double,
+    val totalElevationMeters: Int
+)
